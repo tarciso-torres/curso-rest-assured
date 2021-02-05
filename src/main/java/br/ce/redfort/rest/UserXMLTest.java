764eb008b9998863d7f402ce3aa05b6f609b0d51
Tarciso Torres <tarcisoftorres@gmail.com>
2021-02-05 09:12:21 -0300

    test: deve trabalhar com xml

---
+++ src/main/java/br/ce/redfort/rest/UserXMLTest.java
@@ -0,0 +1,35 @@
+package br.ce.redfort.rest;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.junit.Test;
+
+public class UserXMLTest {
+	
+	@Test
+	public void devoTrabalharComXML() {
+		given()
+		.when()
+			.get("https://restapi.wcaquino.me/usersXML/3")
+		.then()
+			.statusCode(200)
+			.rootPath("user")
+			.body("name", is("Ana Julia"))
+			.body("@id", is("3"))
+			
+			.rootPath("user.filhos")
+			.body("name.size()", is(2))
+			
+			.detachRootPath("filhos")
+			.body("filhos.name[0]", is("Zezinho"))
+			.body("filhos.name[1]", is("Luizinho"))
+			
+			.appendRootPath("filhos")
+			.body("name", hasItem("Zezinho"))
+			.body("name", hasItems("Zezinho", "Luizinho"))
+		;
+		
+	}
+
+}
