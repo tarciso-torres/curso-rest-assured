26d3350be26491894813c1cb66aee2a4d0297a9c
Tarciso Torres <tarcisoftorres@gmail.com>
2021-02-05 16:53:18 -0300

    test: deve fazer pesquisas avançadas com XML

--- src/main/java/br/ce/redfort/rest/UserXMLTest.java
+++ src/main/java/br/ce/redfort/rest/UserXMLTest.java
@@ -31,5 +31,23 @@ public class UserXMLTest {
 		;
 		
 	}
+	
+	@Test
+	public void devoFazerPesquisasAvancadasComXML() {
+		given()
+		.when()
+			.get("https://restapi.wcaquino.me/usersXML")
+		.then()
+			.statusCode(200)
+			.body("users.user.size()", is(3))
+			.body("users.user.findAll{it.age.toInteger() <= 25}.size()", is(2))
+			.body("users.user.@id", hasItems("1", "2", "3"))
+			.body("users.user.find{it.age == 25}.name", is("Maria Joaquina"))
+			.body("users.user.findAll{it.name.toString().contains('n')}.name", hasItems("Maria Joaquina", "Ana Julia"))
+			.body("users.user.salary.find{it != null}.toDouble()", is(1234.5678d))
+			.body("users.user.name.findAll{it.toString().startsWith('Maria')}.collect{it.toString().toUpperCase()}", is("MARIA JOAQUINA"));
+		;
+		
+	}
 
 }
