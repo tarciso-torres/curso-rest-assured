cc85e0de6718297ab0fb525b8e4ed7f7e9da6990
Tarciso Torres <tarcisoftorres@gmail.com>
2021-02-06 16:23:41 -0300

    test: deve salvar usuário

---
+++ src/main/java/br/ce/redfort/rest/VerbosTest.java
@@ -0,0 +1,27 @@
+package br.ce.redfort.rest;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import org.junit.Test;
+
+public class VerbosTest {
+
+	@Test
+	public void deveSalvarUsuario() {
+		given()
+			.log().all()
+			.contentType("application/json")
+			.body("{\"name\": \"Jose\", \"age\": 50}")
+		.when()
+			.post("https://restapi.wcaquino.me/users")
+		.then()
+			.log().all()
+			.statusCode(201)
+			.body("id", is(notNullValue()))
+			.body("name", is("Jose"))
+			.body("age", is(50))
+		;
+	}
+	
+}
