0eaeddeee69847dab92affe4600dba82fb12b52a
Tarciso Torres <tarcisoftorres@gmail.com>
2021-02-07 15:01:00 -0300

    test: deve fazer buscas com Xpath em HTML

--- src/main/java/br/ce/redfort/rest/HTML.java
+++ src/main/java/br/ce/redfort/rest/HTML.java
@@ -1,7 +1,7 @@
 package br.ce.redfort.rest;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 
 import org.junit.Test;
 
@@ -23,6 +23,21 @@ public class HTML {
 			.body("html.body.div.table.tbody.tr[1].td[2]", is("25"))
 			.appendRootPath("html.body.div.table.tbody")
 			.body("tr.find{it.toString().startsWith('2')}.td[1]", is("Maria Joaquina"))
+		;
+	}
+	
+	@Test
+	public void deveFazerBuscasComXpathEmHTML() {
+		given()
+			.log().all()
+		.when()
+			.get("https://restapi.wcaquino.me/v2/users?format=clean")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.contentType(ContentType.HTML)
+			.body(hasXPath("count(//table/tr)", is("4")))
+			.body(hasXPath("//td[text() = '2']/../td[2]", is("Maria Joaquina")))
 		;
 	}
 
