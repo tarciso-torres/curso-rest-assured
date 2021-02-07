212ef63a10943c2009591303a9ac3a62261e4219
Tarciso Torres <tarcisoftorres@gmail.com>
2021-02-07 13:22:49 -0300

    test: deve salvar usuário usando Objeto

---
+++ src/main/java/br/ce/redfort/rest/User.java
@@ -0,0 +1,39 @@
+package br.ce.redfort.rest;
+
+public class User {
+
+	private String name;
+	private Integer age;
+	private Double salary;
+
+	public User(String name, Integer age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+}
--- src/main/java/br/ce/redfort/rest/VerbosTest.java
+++ src/main/java/br/ce/redfort/rest/VerbosTest.java
@@ -49,6 +49,25 @@ public class VerbosTest {
 			.body("age", is(25))
 		;
 	}
+
+	@Test
+	public void deveSalvarUsuarioUsandoObjeto() {
+		User user = new User("Usuário via objeto", 35);
+		
+		given()
+			.log().all()
+			.contentType("application/json")
+			.body(user)
+		.when()
+			.post("https://restapi.wcaquino.me/users")
+		.then()
+			.log().all()
+			.statusCode(201)
+			.body("id", is(notNullValue()))
+			.body("name", is("Usuário via objeto"))
+			.body("age", is(35))
+		;
+	}
 	
 	@Test
 	public void naoDeveSalvarUsuarioSemNome() {
