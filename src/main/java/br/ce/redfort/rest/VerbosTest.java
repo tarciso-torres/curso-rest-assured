f8d4f55c36553a4a48cb7c97c0fd452add5ad8c5
Tarciso Torres <tarcisoftorres@gmail.com>
2021-02-07 12:54:29 -0300

    test: deve customizar url parte 2

--- src/main/java/br/ce/redfort/rest/VerbosTest.java
+++ src/main/java/br/ce/redfort/rest/VerbosTest.java
@@ -75,4 +75,24 @@ public class VerbosTest {
 			.body("salary", is(1234.5678f))
 		;
 	}
+
+	@Test
+	public void deveCustomizarURLParte2() {
+		given()
+			.log().all()
+			.contentType("application/json")
+			.body("{\"name\": \"Usuário Alterado\", \"age\": 80}")
+			.pathParam("entidade", "users")
+			.pathParam("id", "1")
+		.when()
+			.put("https://restapi.wcaquino.me/{entidade}/{id}")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body("id", is(1))
+			.body("name", is("Usuário Alterado"))
+			.body("age", is(80))
+			.body("salary", is(1234.5678f))
+		;
+	}
 }
