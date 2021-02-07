3264e807f3b4ad506129173ec063d765a7a64029
Tarciso Torres <tarcisoftorres@gmail.com>
2021-02-07 15:36:53 -0300

    test: deve baixar arquivo

--- src/main/java/br/ce/redfort/rest/FileTest.java
+++ src/main/java/br/ce/redfort/rest/FileTest.java
@@ -4,7 +4,11 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class FileTest {
@@ -45,8 +49,28 @@ public class FileTest {
 			.post("https://restapi.wcaquino.me/upload")
 		.then()
 			.log().all()
+			.time(lessThan(10000l))
 			.statusCode(413)
 		;
+	}
+	
+	@Test
+	public void deveBaixarArquivo() throws IOException {
+		byte[] image = given()
+			.log().all()
+		.when()
+			.get("https://restapi.wcaquino.me/download")
+		.then()
+			.statusCode(200)
+			.extract().asByteArray()
+		;
+		
+		File imagem = new File("src/main/resources/file.jpg");
+		OutputStream out = new FileOutputStream(imagem);
+		out.write(image);
+		out.close();
+		
+		Assert.assertThat(imagem.length(), lessThan(100000L));
 	}
 
 }
