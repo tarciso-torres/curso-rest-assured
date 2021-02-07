34ef26926f9c90ce6286748e77ee637dc276d096
Tarciso Torres <tarcisoftorres@gmail.com>
2021-02-07 14:49:45 -0300

    test: deve fazer buscas com HTML

---
+++ src/main/java/br/ce/redfort/rest/HTML.java
@@ -0,0 +1,29 @@
+package br.ce.redfort.rest;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Test;
+
+import io.restassured.http.ContentType;
+
+public class HTML {
+	
+	@Test
+	public void deveFazerBuscasComHTML() {
+		given()
+			.log().all()
+		.when()
+			.get("https://restapi.wcaquino.me/v2/users")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.contentType(ContentType.HTML)
+			.body("html.body.div.table.tbody.tr.size()", is(3))
+			.body("html.body.div.table.tbody.tr[1].td[2]", is("25"))
+			.appendRootPath("html.body.div.table.tbody")
+			.body("tr.find{it.toString().startsWith('2')}.td[1]", is("Maria Joaquina"))
+		;
+	}
+
+}
