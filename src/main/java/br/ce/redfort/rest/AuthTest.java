962db98e9bb9ff10bd9932ed0ebb645aeac30fba
Tarciso Torres <tarcisoftorres@gmail.com>
2021-02-09 19:13:57 -0300

    test: deve acessar aplicação Web

--- src/main/java/br/ce/redfort/rest/AuthTest.java
+++ src/main/java/br/ce/redfort/rest/AuthTest.java
@@ -11,6 +11,8 @@ import java.util.Map;
 import org.junit.Test;
 
 import io.restassured.http.ContentType;
+import io.restassured.path.xml.XmlPath;
+import io.restassured.path.xml.XmlPath.CompatibilityMode;
 
 public class AuthTest {
 	
@@ -126,6 +128,44 @@ public class AuthTest {
 			.log().all()
 			.body("nome", hasItem("teste"))
 		;
+	}
+	
+	@Test
+	public void deveAcessarAplicacaoWeb() {
+		//login
+		String cookie = given()
+			.log().all()
+			.formParam("email", "wagner@aquino")
+			.formParam("senha", "123456")
+			.contentType(ContentType.URLENC.withCharset("UTF-8"))
+		.when()
+			.post("http://seubarriga.wcaquino.me/logar")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.extract().header("set-cookie");
+		;
+		
+		cookie = cookie.split("=")[1].split(";")[0];
+		System.out.println(cookie);
+		
+		//obter conta
+		
+		String body = given()
+			.log().all()
+			.cookie("connect.sid", cookie)
+		.when()
+			.get("http://seubarriga.wcaquino.me/contas")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body("html.body.table.tbody.tr[0].td[0]", is("Conta mesmo nome"))
+			.extract().body().asString();
+		;
+		
+		System.out.println("--------------");
+		XmlPath xmlPath = new XmlPath(CompatibilityMode.HTML, body);
+		System.out.println(xmlPath.getString("html.body.table.tbody.tr[0].td[0]"));
 	}
 	
 }
